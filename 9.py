d79e49e9704beaf207b4275e1f511c3920caef1a
DYusL <dyl9293@gmail.com>
2016-12-26 18:25:27 +0100

    Add problem 9 solved

---
+++ 9.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def calcPerfectSquare():
+    for a in range(1,1001):
+        for b in range(1,1001):
+            c_2 = a**2 + b**2
+            c = np.sqrt(c_2)
+            if c % 1 == 0:
+                if 1000 == a + b + c:
+                    return a, b, c, (a*b*c)
+
+print(calcPerfectSquare())
+
+
+
+
+
+
